2aac4938057a3bc4a829dacad2072585109466a5
Oliver <ob124.oliver@gmail.com>
2020-10-24 16:46:17 +1300

    Fixed the remaining login bug due to some copy paste errors.

--- Movies/register/register.py
+++ Movies/register/register.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, flash
-from flask import request, render_template, redirect, session
+from flask import request, render_template, redirect, session, url_for
 import Movies.adapters.repository as repo
 
 register_blueprint = Blueprint('register_bp', __name__)
@@ -12,19 +12,16 @@ def register():
         username = request.form['username']
         password = request.form['password']
         # see if credentials are valid and that the user doesn't already exist
-        if len(username) >= 3 and len(password) >= 6 and username not in repo.repo_instance.get_user_names:
+        if len(username) >= 3 and len(password) >= 6 and username not in repo.repo_instance.get_user_names():
             session['login'] = username
             repo.repo_instance.add_user(username, password)
             return redirect('/')
         else:
-            if username in repo.repo_instance.get_user_names:
-                flash("Username taken.")
-                return redirect('/register')
+            if username in repo.repo_instance.get_user_names():
+                return redirect(url_for("register_bp.register"))
             if len(password) < 6:
-                flash("please ensure your password is at least 6 characters long.")
-                return redirect('/register')
+                return redirect(url_for("register_bp.register"))
             else:
-                flash("please ensure your username is at least 3 characters long.")
-                return redirect('/register')
+                return redirect(url_for("register_bp.register"))
 
     return render_template('login/register.html')
